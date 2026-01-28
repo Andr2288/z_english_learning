f08614f67cff0a53f147d901ac049f784d5f2127
chorn <chornookyiandry228@gmail.com>
2026-01-28 21:54:04 +0200

    Migrating to VITE Complete

---
+++ vite_project/src/App.jsx
@@ -0,0 +1,11 @@
+import Exercise from "../src/components/exercises/Exercise.jsx";
+
+function App() {
+    return (
+        <div className="container flex items-center justify-center mx-auto min-h-screen bg-gradient-to-br from-slate-100 via-blue-50 to-indigo-100">
+            <Exercise />
+        </div>
+    );
+}
+
+export default App;
